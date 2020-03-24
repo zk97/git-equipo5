a2aca42a8203ede336abcd1695a6ad64a856bc99
Zahid Sanchez <10248069+zk97@users.noreply.github.com>
2020-03-24 01:37:37 -0600

    first edit

--- multiple.sequences.py
+++ multiple.sequences.py
@@ -3,6 +3,8 @@ people = ['Jonas', 'Julio', 'Mike', 'Mez']
 
 ages = [25, 30, 31, 39]
 
+print("this is the original message")
+
 for position in range(len(people)):
     person = people[position]
     age = ages[position]
