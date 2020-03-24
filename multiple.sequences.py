bb9d3ef9851dde166dd638b7e2a8de4bff688882
Zahid Sanchez <10248069+zk97@users.noreply.github.com>
2020-03-24 01:42:08 -0600

    final changes?

--- multiple.sequences.py
+++ multiple.sequences.py
@@ -3,7 +3,7 @@ people = ['Jonas', 'Julio', 'Mike', 'Mez']
 
 ages = [25, 30, 31, 39]
 
-print("this is the altered message")
+print("this is the master message")
 
 for position in range(len(people)):
     person = people[position]
